e47be31a914879c0b6829864348853d871f4710e
Andrew Johnson <andrew.johnson@maketime.io>
2015-05-28 09:36:56 -0400

    Prepare for fork

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-$('li').click(function(){
+$(document).ready(function(){
+	$('li').click(function(){
   $(this).toggleClass('active');
 	$(this).find('ul').toggleClass('active');
 });
 $('small').click(function(){  $(this).find('.changelog').toggleClass('active');
+});
 });
---
+++ js/min/index-min.js
@@ -0,0 +1,1 @@
+$("li").click(function(){$(this).toggleClass("active"),$(this).find("ul").toggleClass("active")}),$("small").click(function(){$(this).find(".changelog").toggleClass("active")});
